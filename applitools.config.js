2af9ce54586ae65ebb7f33554327a9a09ad3d441
Frankie Valentine <frankievalentine9@gmail.com>
2021-05-03 15:26:12 -0700

    add applitools eyes

---
+++ applitools.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  browser: [
+    { width: 800, height: 600, name: "firefox" },
+    { width: 1024, height: 768, name: "chrome" },
+    { width: 1024, height: 768, name: "ie11" },
+  ],
+};
